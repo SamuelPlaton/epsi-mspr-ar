c5969ed74fe685040960c028fd0c76461af324a5
Samuel Platon <platonsam02@gmail.com>
2022-02-02 12:12:04 +0100

    test: set e2e and interface tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,26 @@
 /**
  * @format
  */
-
 import React from 'react';
 import 'react-native';
 import renderer from 'react-test-renderer';
 import App from '../App';
+import { CameraPage } from '../src/pages';
 
-// Note: test renderer must be required after react-native.
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+describe('App', () => {
+  it('renders correctly', () => {
+    renderer.create(<App />);
+  });
+  it('Display 3D model', () => {
+    // Mock camera display
+    jest.mock('react-native-camera', () => {
+      const mockComponent = require('react-native/jest/mockComponent');
+      return mockComponent('./drawing.jpg');
+    });
+    const data = renderer.create(<CameraPage />).toJSON();
+    // Check that our 3D model is correctly returned
+    expect(data).toContainAnyKeys('3d-model');
+  })
+})
+
