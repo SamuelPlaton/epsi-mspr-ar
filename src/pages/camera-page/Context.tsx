ea1d36d938fe72e2b2ccfff76a969a3e4ae45c8f
Samuel Platon <platonsam02@gmail.com>
2022-02-08 12:17:15 +0100

    feat: add image to glview

--- src/pages/camera-page/Context.tsx
+++ src/pages/camera-page/Context.tsx
@@ -1,20 +1,37 @@
 import { GLView } from 'expo-gl';
 import React from 'react';
+import { View, Text } from 'react-native';
 import Expo2DContext from 'expo-2d-context';
+import { Asset } from 'expo-asset';
 
 export default class Context extends React.Component {
   render() {
     return (
-      <GLView style={{ flex: 1 }} onContextCreate={this._onGLContextCreate} />
+      <GLView
+        style={{
+          flex: 1, width: 1000, height: 1000, zIndex: 200,
+        }}
+        onContextCreate={this._onGLContextCreate}
+      />
     );
   }
+
   // example here : https://www.npmjs.com/package/expo-2d-context
-  _onGLContextCreate = (gl) => {
+  _onGLContextCreate = async (gl) => {
     const ctx = new Expo2DContext(gl);
-    ctx.fillStyle = 'purple';
-    ctx.fillRect(200, 200, 20, 30);
+    const asset = Asset.fromModule(require('./dog.png'));
+    const dlAsset = await asset.downloadAsync();
+    const pattern = ctx.createPattern(dlAsset, 'repeat');
+    ctx.fillStyle = pattern;
+    ctx.fillRect(200, 200, 200, 300);
+    ctx.stroke();
+    ctx.flush();
     const imgData = ctx.getImageData(200, 200, 1, 1);
     console.log('PURPLE COLOR : ');
     console.log(imgData.data);
+    const viewData = ctx.getImageData(100, 100, 1, 1);
+    console.log('RED COLOR : ');
+    console.log(viewData.data);
+    return gl;
   };
 }
